98e844e0ffd904a5635496544bdf7b6fc9694f39
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-12-23 18:15:27 -0700

    updates documentation

--- R/use_github_enterprise.R
+++ R/use_github_enterprise.R
@@ -1,11 +1,42 @@
 #' Use GitHub Enterprise components in your package
 #'
+#' These functions are used to write function files for your package.
+#'
+#' The function `use_ghe_enterprise()` calls all the templating functions:
+#'
+#' \describe{
+#'   \item{`use_install_ghe()`}{Writes a file, `utils-install_ghe.R` containing
+#'     a function `install_github_foo()` that wraps
+#'     `devtools::install_github()`, using the `host` argument of this function.}
+#'   \item{`use_ghe_pat()`}{Writes a file, `utils-ghe_pat.R` containing
+#'     a function `github_foo_pat()` that parallels `devtools::github_pat()`.
+#'     This new function will look for an environment variable
+#'     named `GITHUB_FOO_PAT`.}
+#'   \item{`use_use_ghe()`}{Writes a file, `utils-use_ghe.R`, that
+#'     contains a function `use_github_foo()` that
+#'     wraps `usethis::use_github()`, using
+#'     the `host` argument of this function.}
+#' }
+#'
+#' For each of the functions described above, the `foo` in the function name
+#' is the value of the `suffix` argument from this function.
+#' The `name` argument from this function is interpolated into the
+#' documentation of each of written functions.
+#'
 #' @param suffix `character` lowercase suffix to use for function names
 #' @param name `character` title-case name of the GHE instance
 #' @param host `character` hostname for GHE instance
 #'
 #' @return `invisible(NULL)`, called for side effects
 #' @export
+#' @examples
+#' \dontrun{
+#'   use_github_enterprise(
+#'     host = "github.acme-corp.com/api/v3",
+#'     suffix = "acme",
+#'     name = "Acme Corporation"
+#'   )
+#' }
 #'
 use_github_enterprise <- function(host, suffix, name) {
 
@@ -47,9 +78,7 @@ use_github_enterprise <- function(host, suffix, name) {
 #'
 use_install_ghe <- function(host, suffix, name) {
 
-  if (!usethis:::uses_roxygen()) {
-    stop("`use_install_ghe()` requires that you use roxygen.", call. = FALSE)
-  }
+  usethis::use_roxygen_md()
 
   data <- list(
     host = host,
@@ -73,9 +102,7 @@ use_install_ghe <- function(host, suffix, name) {
 #'
 use_ghe_pat <- function(suffix, name) {
 
-  if (!usethis:::uses_roxygen()) {
-    stop("`use_ghe_pat()` requires that you use roxygen.", call. = FALSE)
-  }
+  usethis::use_roxygen_md()
 
   data <- list(
     suffix = suffix,
@@ -101,9 +128,7 @@ use_ghe_pat <- function(suffix, name) {
 #'
 use_use_ghe <- function(host, suffix, name) {
 
-  if (!usethis:::uses_roxygen()) {
-    stop("`use_use_ghe()` requires that you use roxygen.", call. = FALSE)
-  }
+  usethis::use_roxygen_md()
 
   data <- list(
     host = host,
